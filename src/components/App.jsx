730f13360d01f60d460b64bb8d485b00b9aa8ac0
nadine525 <02_seeders_wide@icloud.com>
2023-06-05 14:46:11 +0200

    section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import Section from './Section/Section';
+
+export class App extends Component {
+
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback">
+        {/* <FeedbackOptions /> */}
+      </Section>
+      </div>
+    )
+  
+  };
 };
---
+++ src/components/Section/Section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Division } from './Section.styled';
+
+const Section = ({ title }) => {
+  <Division>
+    <h2>{title}</h2>
+    {/* {children} */}
+  </Division>;
+};
+
+export default Section;
